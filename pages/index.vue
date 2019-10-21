8952c72b86541d8b986bf6e111dae065e73b0359
tanaka.mio <dcm.soe.mio.tanaka@gmail.com>
2019-10-21 18:51:38 +0900

    20191021_work1

--- pages/index.vue
+++ pages/index.vue
@@ -1,86 +1,88 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        nuxt-ts
-      </h1>
-      <h2 class="subtitle">
-        {{ $vxm.users.fullname }}'s swell Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
+  <div>
+    <div class="menu">
+      <input type="text" value="あなたの名前" />
+      <button>GAME START!!!</button>
+      <button>STOP!!!</button>
+    </div>
+    <div class="messageArea">○○さん {{ message }}</div>
+    <div class="board">
+      <template v-for="y in board.length">
+        <div
+          v-for="x in board[y - 1].length"
+          :key="`${x}-${y}`"
+          class="cell"
+          @click="onClick(x, y)"
         >
-          GitHub
-        </a>
-      </div>
+          <div
+            v-if="board[y - 1][x - 1] !== 0"
+            :class="['ball', board[y - 1][x - 1] === 1 ? 'white' : 'black']"
+          />
+        </div>
+      </template>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue, Prop } from 'nuxt-property-decorator'
-import Logo from '~/components/atoms/Logo.vue'
+import { Vue } from 'nuxt-property-decorator'
 
-@Component({
-  components: { Logo }
-})
 export default class extends Vue {
-  @Prop()
-  test!: {
-    hoge: string
-  }
+  board: number[][] = [
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, -1, 0, 0, 0],
+    [0, 0, 0, -1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0]
+  ]
 
-  async mounted() {
-    await this.$vxm.users.doAnotherAsyncStuff(4)
+  message: string = 'あなたの番です'
+
+  public onClick(x: number, y: number) {
+    alert(x + 'と' + y)
   }
 }
 </script>
 
 <style scoped>
-.container {
+.menu {
+  margin: 5%;
+}
+
+.messageArea {
+  margin: 0 5%;
+}
+
+.board {
   margin: 0 auto;
-
-  /* Caution: `min-height: 100vh` does not work in IE 11 */
-  height: 100vh;
-  display: grid;
-
-  /* Caution: `1fr` has different results in IE 11 */
-  grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: repeat(3, 1fr);
-  text-align: center;
+  width: 640px;
+  height: 640px;
+  background: #060;
 }
 
-.container > div {
-  grid-column: 2 / 3;
-  grid-row: 2 / 3;
+.cell {
+  width: 12.5%;
+  height: 12.5%;
+  border: black solid 1px;
+  float: left;
+  list-style: none;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.ball {
+  width: 80%;
+  height: 80%;
+  border-radius: 50%;
+  margin: 10%;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.black {
+  background: black;
 }
 
-.links {
-  padding-top: 15px;
+.white {
+  background: white;
 }
 </style>
