8599e733b037256cc207aba7e03a64e40e0d0691
tanaka.mio <dcm.soe.mio.tanaka@gmail.com>
2019-11-01 18:33:36 +0900

    20191101_work1

---
+++ components/molecules/Calculation.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="calculator">
+    <input v-model.number="numA" type="text" placeholder="数字" />
+    <select v-model="operatorId">
+      <option
+        v-for="operator in operators"
+        :key="operator.id"
+        :value="operator.id"
+        >{{ operator.label }}</option
+      >
+    </select>
+    <input v-model.number="numB" type="text" placeholder="数字" />
+    <button @click="onChildClick(numA, numB)">
+      Answer
+    </button>
+    <br />
+    A. {{ answer }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'nuxt-property-decorator'
+
+@Component
+export default class Calculation extends Vue {
+  @Prop()
+  answer!: {
+    type: Number
+  }
+  operators!: {
+    type: Object
+  }
+
+  // 計算機用
+  numA = 0
+  numB = 0
+  operatorId = 0
+
+  // 計算機能用クリックイベント
+  public onChildClick(numA: number, numB: number) {
+    this.$emit('calculator', numA, numB, 1)
+  }
+}
+</script>
+
+<style scoped>
+.calculator {
+  margin: 0 auto;
+  padding: 10px;
+  width: 640px;
+  background: lightblue;
+}
+</style>
--- components/organisms/Calculator.vue
+++ components/organisms/Calculator.vue
@@ -1,45 +1,22 @@
 <template>
-  <div class="calculator">
-    <input v-model.number="numA" type="text" placeholder="数字" />
-    <select v-model="operatorId">
-      <option
-        v-for="operator in $vxm.calculator.operators"
-        :key="operator.id"
-        :value="operator.id"
-        >{{ operator.label }}</option
-      >
-    </select>
-    <input v-model.number="numB" type="text" placeholder="数字" />
-    <button @click="onAnswerClick(numA, numB)">
-      Answer
-    </button>
-    <br />
-    A. {{ $vxm.calculator.calculatorAnswer }}
-  </div>
+  <Calculation
+    :answer="this.$vxm.calculator.calculatorAnswer"
+    :operators="this.$vxm.calculator.operators"
+    @calculator="onAnswerClick"
+  />
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'nuxt-property-decorator'
+import Calculation from '~/components/molecules/Calculation.vue'
 
-@Component
+@Component({
+  components: { Calculation }
+})
 export default class Calculator extends Vue {
-  // 計算機用
-  numA = 0
-  numB = 0
-  operatorId = 0
-
   // 計算機能用クリックイベント
-  public onAnswerClick(numA: number, numB: number) {
-    this.$vxm.calculator.setAnswer({ numA, numB, operatorId: this.operatorId })
+  public onAnswerClick(numA: number, numB: number, operatorId: number) {
+    this.$vxm.calculator.setAnswer({ numA, numB, operatorId })
   }
 }
 </script>
-
-<style scoped>
-.calculator {
-  margin: 0 auto;
-  padding: 10px;
-  width: 640px;
-  background: lightblue;
-}
-</style>
